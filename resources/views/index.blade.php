ea2d9d2ace113bc0f300277d73b19cf7500deda2
Desry Hernalin <desryhernalin@gmail.com>
2019-01-07 22:55:26 +0800

    Adde CSS files and homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layout')
+
+<div class="container">
+	<div id="storeWrapper">
+		<h1 id="storeName">Marianne's Closet</h1>
+		<center><a class="btn waves-effect waves-light black btn-large" href="#">Shop</a></center>
+	</div>
+</div>
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Marianne's Closet</title>
+	<meta charset="utf-8">
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="css/materialize-social.css">
+    <link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.css"> 	
+    <link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="css/mycss.css">
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+    <script src='js/jquery2-1-1.js'></script>
+    <script type="text/javascript" src="js/materialize.js"></script>
+    <script type="text/javascript" src="js/init.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+</head>
+<body>
+</head>
+<body>
+
+	
+	<div id="container">
+		@yield('content')
+	</div>
+
+
+</body>
+</html>
